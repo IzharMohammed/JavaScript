9e25f717d9371e353cfceccdf12ce0542a06a89d
IzharMohammed <izharmohammed21@gmail.com>
2025-12-16 12:42:29 +0530

    Polyfill explained

--- polyfill.js
+++ polyfill.js
@@ -1,44 +1,125 @@
-// Ployfill for map()
-//  Array.map((num,i,arr)=>{ })
+/*
+============================================================
+ POLYFILLS: map(), filter(), reduce()
+============================================================
+
+WHAT IS A POLYFILL?
+- A polyfill is a custom implementation of a JS feature
+  that may not exist in older browsers.
+- Interviewers ask polyfills to test:
+  ✔ Understanding of array methods
+  ✔ Callbacks
+  ✔ `this` keyword
+  ✔ Loops & edge cases
+*/
+
+const nums = [1, 2, 3, 4, 5];
+
+// -----------------------------------------------------------
+// 1) POLYFILL FOR map()
+// -----------------------------------------------------------
+
+// Original syntax:
+// Array.map((value, index, array) => {})
+
+// THEORY:
+// - map returns a NEW array
+// - Same length as original
+// - Callback is applied to every element
 
 Array.prototype.myMap = function (cb) {
   let temp = [];
+
   for (let i = 0; i < this.length; i++) {
+    // this -> refers to the array on which myMap is called
     temp.push(cb(this[i], i, this));
   }
+
   return temp;
 };
 
-const nums = [1, 2, 3, 4, 5];
-let MultiplyByTwo = nums.myMap((num, i) => num * 2);
-console.log(MultiplyByTwo);
-// [ 2, 4, 6, 8, 10 ]
+const multiplyByTwo = nums.myMap((num, i) => {
+  return num * 2;
+});
 
-// polyfill for filter()
+console.log("myMap result:", multiplyByTwo);
+// Output: [2, 4, 6, 8, 10]
+
+// -----------------------------------------------------------
+// 2) POLYFILL FOR filter()
+// -----------------------------------------------------------
+
+// Original syntax:
+// Array.filter((value, index, array) => boolean)
+
+// THEORY:
+// - filter returns a NEW array
+// - Length may be smaller
+// - Callback must return true to keep element
+
 Array.prototype.myFilter = function (cb) {
   let temp = [];
+
   for (let i = 0; i < this.length; i++) {
-    if (cb(this[i], i, this)) temp.push(this[i]);
+    if (cb(this[i], i, this)) {
+      temp.push(this[i]);
+    }
   }
+
   return temp;
 };
 
-const moreThanTwo = nums.myFilter((num) => num > 3);
-console.log(moreThanTwo);
-// [4,5]
+const moreThanThree = nums.myFilter((num) => num > 3);
 
-// Polyfill for reduce
-// arr.reduce((acc,curr,i,arr)=>{},initialValue)
+console.log("myFilter result:", moreThanThree);
+// Output: [4, 5]
+
+// -----------------------------------------------------------
+// 3) POLYFILL FOR reduce()
+// -----------------------------------------------------------
+
+// Original syntax:
+// Array.reduce((acc, curr, index, array) => {}, initialValue)
+
+// THEORY:
+// - reduce returns a SINGLE value
+// - accumulator stores the result
+// - initialValue is optional but IMPORTANT
 
 Array.prototype.myReduce = function (cb, initialValue) {
-  var accumulator = initialValue;
+  let accumulator = initialValue;
+
   for (let i = 0; i < this.length; i++) {
-    accumulator = accumulator ? cb(accumulator, this[i], i, this) : this[i];
+    if (accumulator !== undefined) {
+      accumulator = cb(accumulator, this[i], i, this);
+    } else {
+      // If no initial value, first element becomes accumulator
+      accumulator = this[i];
+    }
   }
+
   return accumulator;
 };
 
-const sum = nums.reduce((acc, curr) => {
+const sumUsingMyReduce = nums.myReduce((acc, curr) => {
   return acc + curr;
 }, 0);
-console.log(sum); // 15
+
+console.log("myReduce result (sum):", sumUsingMyReduce);
+// Output: 15
+
+// -----------------------------------------------------------
+// IMPORTANT INTERVIEW NOTES
+// -----------------------------------------------------------
+
+/*
+1) Why we use Array.prototype?
+   - So that method is available on all arrays
+
+2) Why NOT use arrow function for polyfill?
+   - Arrow functions do NOT have their own `this`
+   - We need `this` to refer to the array
+
+3) reduce edge case:
+   - Always handle missing initialValue
+*/
---
+++ questions.js
@@ -0,0 +1,6 @@
+// Question 1: map vs forEach
+const arr = [2, 4, 6, 8, 10];
+
+arr.map((i) => console.log(i));
+
+arr.forEach((i) => console.log(i));
